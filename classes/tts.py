92789e39e9378ef1df653d31eeca78c3b907e989
isharab <155580193+sherijigarx@users.noreply.github.com>
2024-01-13 16:52:12 +0500

    Update tts.py

--- classes/tts.py
+++ classes/tts.py
@@ -68,8 +68,9 @@ class TextToSpeechService(AIModelService):
                 await self.main_loop_logic(step)
                 step += 1
                 await asyncio.sleep(0.5)  # Adjust the sleep time as needed
-                if step % 500 == 0 and self.config.auto_update == "yes":
+                if step % 50 == 0 and self.config.auto_update == "yes":
                     lib.utils.try_update()
+                bt.logging.info(f"_________________ Current step is _________________{step}")
             except KeyboardInterrupt:
                 print("Keyboard interrupt detected. Exiting TextToSpeechService.")
                 break
