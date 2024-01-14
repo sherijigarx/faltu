0357eb10246ea09b923de47b5865f1665aea2868
shaukathussain <shaukathussain1@hotmail.com>
2024-01-14 16:13:03 +0100

    fix voice clone model

--- models/bark_voice_clone.py
+++ models/bark_voice_clone.py
@@ -18,20 +18,9 @@ sys.path.insert(0, audio_subnet_path)
 # from bark_extras import HuBERTManager, CustomHubert, CustomTokenizer, load_codec_model, generate_text_semantic, preload_models, codec_decode, generate_coarse, generate_fine 
 from encodec.utils import convert_audio
 
-
-
-
-
-
-
-
-
-
-
 import gc
 import os
 import re
-import json
 import json
 import numpy
 import math
@@ -1610,78 +1599,143 @@ def codec_decode(fine_tokens):
 
 
 
-class BarkVoiceClone:
-    def __init__(self):
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        # self.input_audio_file = input_audio_file
-        self.model = self.load_codec_model()
-        self.hubert_manager = self.load_hubert_manager()
-        self.hubert_model = self.load_hubert_model()
-        self.tokenizer = self.load_tokenizer()
+class ModelLoader:
+   def _init_(self, device):
+       self.device = device
+       self.model = self.load_codec_model()
+       self.hubert_manager = self.load_hubert_manager()
+       self.hubert_model = self.load_hubert_model()
+       self.tokenizer = self.load_tokenizer()
 
-    def load_codec_model(self):
-        return load_codec_model(use_gpu=True if self.device == 'cuda' else False)
+   def load_codec_model(self):
+       return load_codec_model(use_gpu=True if self.device == 'cuda' else False)
 
-    def load_hubert_manager(self):
-        hubert_manager = HuBERTManager()
-        hubert_manager.make_sure_hubert_installed()
-        hubert_manager.make_sure_tokenizer_installed()
-        return hubert_manager
+   def load_hubert_manager(self):
+       hubert_manager = HuBERTManager()
+       hubert_manager.make_sure_hubert_installed()
+       hubert_manager.make_sure_tokenizer_installed()
+       return hubert_manager
 
-    def load_hubert_model(self):
-        return CustomHubert(checkpoint_path='data/models/hubert/hubert.pt').to(self.device)
+   def load_hubert_model(self):
+       return CustomHubert(checkpoint_path='data/models/hubert/hubert.pt').to(self.device)
 
-    def load_tokenizer(self):
-        return CustomTokenizer.load_from_checkpoint('data/models/hubert/tokenizer.pth').to(self.device)
+   def load_tokenizer(self):
+       return CustomTokenizer.load_from_checkpoint('data/models/hubert/tokenizer.pth').to(self.device)
+   
 
-    def process_audio(self, input_audio_file):
-        wav, sr = torchaudio.load(input_audio_file)
-        wav = convert_audio(wav, sr, self.model.sample_rate, self.model.channels)
-        wav = wav.to(self.device)
-        return wav
+class AudioProcessor:
+  def _init_(self, filepath, model, device):
+      self.filepath = filepath
+      self.model = model
+      self.device = device
+      self.wav, self.sr = torchaudio.load(self.filepath)
 
-    def generate_semantic_tokens(self, wav):
-        semantic_vectors = self.hubert_model.forward(wav, input_sample_hz=self.model.sample_rate)
-        semantic_tokens = self.tokenizer.get_token(semantic_vectors)
-        return semantic_tokens
+  def process_audio(self):
+      self.wav = convert_audio(self.wav, self.sr, self.model.sample_rate, self.model.channels)
+      self.wav = self.wav.to(self.device)
+      return self.wav
 
-    def encode(self, wav):
-        with torch.no_grad():
-            encoded_frames = self.model.encode(wav.unsqueeze(0))
-        codes = torch.cat([encoded[0] for encoded in encoded_frames], dim=-1).squeeze()
-        return codes
 
-    def clone_voice(self, prompt, voice_name):
-        processed_audio = self.process_audio()
+class SemanticGenerator:
+   def _init_(self, hubert_model, tokenizer, wav, model):
+       self.hubert_model = hubert_model
+       self.tokenizer = tokenizer
+       self.wav = wav
+       self.model = model
 
-        semantic_tokens = self.generate_semantic_tokens(processed_audio)
-        codes = self.encode(processed_audio)
+   def generate_semantic_tokens(self):
+       semantic_vectors = self.hubert_model.forward(self.wav, input_sample_hz=self.model.sample_rate)
+       semantic_tokens = self.tokenizer.get_token(semantic_vectors)
+       return semantic_tokens
 
-        output_path = 'assets/prompts/' + voice_name + '.npz'
+class Encoder:
+   def _init_(self, model, wav):
+       self.model = model
+       self.wav = wav
+
+   def encode(self):
+       with torch.no_grad():
+           encoded_frames = self.model.encode(self.wav.unsqueeze(0))
+       codes = torch.cat([encoded[0] for encoded in encoded_frames], dim=-1).squeeze()
+       return codes
+
+
+class AudioGenerator:
+  def _init_(self, text_prompt, voice_name):
+      self.text_prompt = text_prompt
+      self.voice_name = voice_name
+
+  def generate_audio(self):
+      preload_models(
+          text_use_gpu=True,
+          text_use_small=True,
+          coarse_use_gpu=True,
+          coarse_use_small=True,
+          fine_use_gpu=True,
+          fine_use_small=True,
+          codec_use_gpu=True,
+          force_reload=False,
+          path="models"
+      )
+
+      # Generation with more control
+      x_semantic = generate_text_semantic(
+          self.text_prompt,
+          history_prompt=self.voice_name,
+          temp=0.7,
+          top_k=50,
+          top_p=0.95,
+      )
+
+      x_coarse_gen = generate_coarse(
+          x_semantic,
+          history_prompt=self.voice_name,
+          temp=0.7,
+          top_k=50,
+          top_p=0.95,
+      )
+
+      x_fine_gen = generate_fine(
+          x_coarse_gen,
+          history_prompt=self.voice_name,
+          temp=0.5,
+      )
+
+      audio_array = codec_decode(x_fine_gen)
+
+      return audio_array
+
+
+class BarkVoiceCloning:
+    def __init__(self) -> None:
+        pass
+
+    def clone_voice(self, prompt, voice_name, input_audio_file):
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        model_loader = ModelLoader(device)
+
+        # Process audio
+        audio_processor = AudioProcessor(input_audio_file, model_loader.model, device)
+        processed_audio = audio_processor.process_audio()
+
+        # Generate semantic tokens
+        semantic_generator = SemanticGenerator(model_loader.hubert_model, model_loader.tokenizer, processed_audio, model_loader.model)
+        semantic_tokens = semantic_generator.generate_semantic_tokens()
+
+        # Encode audio
+        encoder = Encoder(model_loader.model, processed_audio)
+        codes = encoder.encode()
+
+        # Directory for saving prompts
+        prompts_directory = 'assets/prompts/'
+        if not os.path.exists(prompts_directory):
+            os.makedirs(prompts_directory)
+
+        # Save prompts
+        output_path = prompts_directory + voice_name + '.npz'
         np.savez(output_path, fine_prompt=codes.cpu(), coarse_prompt=codes[:2, :].cpu(), semantic_prompt=semantic_tokens.cpu())
 
-        preload_models(
-            text_use_gpu=True,
-            text_use_small=False,
-            coarse_use_gpu=True,
-            coarse_use_small=False,
-            fine_use_gpu=True,
-            fine_use_small=False,
-            codec_use_gpu=True,
-            force_reload=False,
-            path="models"
-        )
+        audio_generator = AudioGenerator(prompt, voice_name)
+        audio_array = audio_generator.generate_audio()
 
-        # Generation with more control
-        x_semantic = generate_text_semantic(prompt, history_prompt=voice_name, temp=0.7, top_k=50, top_p=0.95)
-        x_coarse_gen = generate_coarse(x_semantic, history_prompt=voice_name, temp=0.7, top_k=50, top_p=0.95)
-        x_fine_gen = generate_fine(x_coarse_gen, history_prompt=voice_name, temp=0.5)
-
-        audio_array = codec_decode(x_fine_gen)
         return audio_array
-
-
-# # Example usage:
-# input_audio_file = 'path/to/your/audio/file.wav'
-# bark_voice_clone = BarkVoiceClone(input_audio_file)
-# audio_array = bark_voice_clone.clone_voice(prompt="Your prompt here", voice_name="output_voice1")
