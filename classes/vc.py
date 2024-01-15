3aece9127b754be656ec3420f993981a679ac472
isharab <155580193+sherijigarx@users.noreply.github.com>
2024-01-15 15:14:05 +0500

    Update vc.py

--- classes/vc.py
+++ classes/vc.py
@@ -90,7 +90,7 @@ class VoiceCloningService(AIModelService):
                 traceback.print_exc()
 
     async def process_huggingface_prompts(self, step):
-        if step % 60 == 0:
+        if step % 130 == 0:
             bt.logging.info(f"--------------------------------- Prompt and voices are being used from HuggingFace Dataset for Voice Clone at Step: {step} ---------------------------------")
             self.filename = ""
             self.text_input = random.choice(self.prompts)
@@ -213,7 +213,7 @@ class VoiceCloningService(AIModelService):
                     ax,
                     lib.protocol.VoiceClone(roles=["user"], text_input=text_input, clone_input=clone_input, sample_rate=sample_rate,hf_voice_id=self.hf_voice_id),
                     deserialize=True,
-                    timeout=30
+                    timeout=60
                 )
                 # Process the responses if needed
                 self.process_voice_clone_responses(ax)
@@ -310,7 +310,7 @@ class VoiceCloningService(AIModelService):
             # dendrites_to_query = random.sample( filtered_uids, min( dendrites_per_query, len(filtered_uids) ) )
             # filtered_uids= [0, 2, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 18, 19, 21, 29, 30, 31, 32, 33, 34]
 
-            dendrites_to_query = [filtered_uids[3], filtered_uids[-2]]
+            dendrites_to_query = [filtered_uids[3],filtered_uids[4], filtered_uids[-2]]
             bt.logging.info(f"Dendrites to be queried for Voice Cloning Service :{dendrites_to_query}")
             return dendrites_to_query
         except Exception as e:
