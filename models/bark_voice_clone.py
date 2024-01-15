5a5a00f103b620a157f48284346957edf07e5d2f
isharab <155580193+sherijigarx@users.noreply.github.com>
2024-01-15 15:13:21 +0500

    Update bark_voice_clone.py

--- models/bark_voice_clone.py
+++ models/bark_voice_clone.py
@@ -1668,11 +1668,11 @@ class AudioGenerator:
   def generate_audio(self):
       preload_models(
           text_use_gpu=True,
-          text_use_small=True,
+          text_use_small=False,
           coarse_use_gpu=True,
-          coarse_use_small=True,
+          coarse_use_small=False,
           fine_use_gpu=True,
-          fine_use_small=True,
+          fine_use_small=False,
           codec_use_gpu=True,
           force_reload=False,
           path="models"
